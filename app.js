36e7eb74216bd0ca1a80d7233ca2e4b1719a81d1
Erika Wright <erikawright@administrators-MacBook-Pro.local>
2020-05-20 13:51:09 -0400

    added second panel for more fields

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 console.log("app js is running");
+//  Initialize 'Help' tooltip.
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
+// 'Next button'
 $("#calculate").click(function () {
-  const pax = $("#participants").val();
-  console.log(pax);
+  const pax = parseInt($("#participants").val());
+  const avgAnnualSales = $("#avgAnnualSales").val();
+
+  console.log(pax, avgAnnualSales);
 
   $("#form-wrapper").css("display", "none");
+  $("#roi-results").css("display", "block");
 });
