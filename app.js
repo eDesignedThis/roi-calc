e5c199bc39e032b0a0a6bb85c598078e5cec1098
Erika Wright <erikawright@administrators-MacBook-Pro.local>
2020-05-20 21:50:38 -0400

    added illustration to first option

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 console.log("app js is running");
+
+let stepText = $("#step-text");
+
+stepText.html("Step <b>1</b> of 2");
 //  Initialize 'Help' tooltip.
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
 // 'Next button'
-$("#calculate").click(function () {
+$("#nextStep").click(function () {
+  stepText.html("Step <b>2</b> of 2");
   const pax = parseInt($("#participants").val());
   const avgAnnualSales = $("#avgAnnualSales").val();
 
   console.log(pax, avgAnnualSales);
 
   $("#form-wrapper").css("display", "none");
+
+  $("#roi-next-step").css("display", "block");
+});
+
+// Calculate Button'
+$("#calculateRoi").click(function () {
+  $("#roi-next-step").css("display", "none");
   $("#roi-results").css("display", "block");
 });
